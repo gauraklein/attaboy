902a9875a67b1e20c242d89bf4e8b7f6c59f5622
Taylor Blocker <taylorblocker@gmail.com>
2019-10-17 20:55:23 -0500

    comitting migrations

---
+++ db/migrations/20191012125452_000_comments.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("comments", table => {
+    table.increments("id");
+    table.integer("comment_author");
+    table.foreign("comment_author").references("users.id");
+    table.string("content", 1000);
+    table.string("images");
+    table.integer("post_id");
+    table.foreign("post_id").references("posts.id");
+    table.integer("comment_reference");
+    table.foreign("comment_reference").references("comments.id");
+    table.integer("comment_attaboys");
+    table.integer("total_comments");
+    table.string("slug");
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw("DROP TABLE comments");
+};
