307252d9924e8606221fa94a8e2ca0c6392517ed
Taylor Blocker <taylorblocker@gmail.com>
2019-10-29 17:53:09 -0500

    Revert "fixing merge conflicts"
    This reverts commit 6fe03fbf9fa3f35210bc47e95e0f656db9168dfe, reversing
    changes made to 14f6975f243c0431385d6d1247569e9e7ee191bd.

--- modules/attagoryFunctions.js
+++ modules/attagoryFunctions.js
@@ -1,5 +1,5 @@
 const { db } = require("./db/dbConnection");
-const { renderSinglePost} = require('./viewPostFunctions')
+const { renderPost} = require('./viewPostFunctions')
 
 //Add Attagory to DB
 
@@ -14,28 +14,15 @@ function newAttagoryToDB (post) {
 function getRelevantPosts (attagoryID) {
   
   const getRelevantPostsQuery = `
-  SELECT
-	posts.id AS postID,
-	posts.post_slug,
-	posts.title,
-	posts.content,
-	posts.post_attaboys AS total_Attaboys,
-	users.username,
-	users.slug AS user_slug,
-	attagories.attagory_name,
-  attagories.slug AS attagory_slug,
-  attagories.id AS attagoryID
-		FROM users
-			Join posts ON posts.post_author = users.id
-			Join attagories on attagories.id = posts.attagory_id
-  WHERE attagory_id =${attagoryID};
+  SELECT *
+  FROM posts 
+  WHERE attagory_id = ${attagoryID};
 `
   return db.raw(getRelevantPostsQuery)
 }
 
 function getAttagoryID (attagorySlug) {
-  
-  console.log('getAttagoryID function')
+  console.log('getting attagory from DB')
   return db.raw(`
   SELECT *
   FROM attagories
@@ -43,76 +30,16 @@ function getAttagoryID (attagorySlug) {
 }
 
 function renderAttagoryPosts (allPosts) {
-  // console.log('this is the render all posts function', allPosts.rows)
-  return '<ul>' + allPosts.rows.map(renderSinglePost).join('') + '</ul>'
+  // console.log('this is the render all posts function', allPosts)
+  return '<ul>' + allPosts.map(renderPost).join('') + '</ul>'
 }
 
-//rendering attagories 
-
-
-// for select boxes
-
-function renderAttagoriesList (allAttagoriesList) {
-  console.log('this is the render attagories as list function')
-  return allAttagoriesList.map(renderIndividualAttagoryAsOption).join('')
-}
-
-function renderIndividualAttagoryAsOption (individualAttagory) {
-  console.log('this is the render individual attagory function')
-    return `
-      <option value="${individualAttagory.id}">${individualAttagory.attagory_name}</option>
-    `
-}
-
-// for attagories page
-
-
-function renderAllAttagories (attagoryList) {
-
-  return '<ul>' + attagoryList.map(renderIndividualAttagory).join('') + '</ul>'
-
-}
-
-function renderIndividualAttagory (attagory) {
-
-  return `<li>   
-  <div class="card border border-dark cardFix">
-  <div class="card-body border border-dark">
-    <a href="/attagories/${attagory.slug}"><h2>${attagory.attagory_name}</h2></a>
-    <p class="card-text">${attagory.attagory_description}</p>
-    <footer class="blockquote-footer">Total Posts: ${attagory.total_posts}  Total Subscribers: ${attagory.total_subscriptions}</cite></footer>
-      
-    <form action="/subscribe" method="post">
-        <button type="subscribe">Subscribe</button>
-    </form>
-    
-  </div>
-</div>
-</li>`
-
-}
-
-// db query
-const getAllAttagoriesQuery = `
-  SELECT *
-    FROM 
-      attagories;`
-
-function getAllAttagories () {
-  return db.raw(getAllAttagoriesQuery)
-}
 
 module.exports = {
     
     newAttagoryToDB: newAttagoryToDB,
     getRelevantPosts: getRelevantPosts,
     getAttagoryID: getAttagoryID,
-    renderAttagoryPosts: renderAttagoryPosts,
-    renderAttagoriesList: renderAttagoriesList,
-    renderIndividualAttagoryAsOption: renderIndividualAttagoryAsOption,
-    getAllAttagories: getAllAttagories,
-    renderAllAttagories: renderAllAttagories,
-    renderIndividualAttagory: renderIndividualAttagory
-
+    renderAttagoryPosts: renderAttagoryPosts
 
   }
--- modules/newCommentFunctions.js
+++ modules/newCommentFunctions.js
@@ -2,7 +2,6 @@ const { db } = require("./db/dbConnection");
 const uuidv1 = require('uuidv1')
 
 
-<<<<<<< HEAD
 function NewCommentToDB(res) {
   var slug = uuidv1();
   return db.raw("INSERT INTO comments (comment_author, content, slug, post_id) VALUES (?, ?, ?, ?)", [
@@ -10,16 +9,6 @@ function NewCommentToDB(res) {
     res.body.content,
     slug,
     res.body.postid
-=======
-function NewCommentToDB(comment) {
-  // console.log(comment.user)
-  slug = uuidv1();
-  return db.raw("INSERT INTO comments (post_id, comment_author, content, slug) VALUES (?, ?, ?, ?)", [
-    comment.body.postID,
-    comment.user.id,
-    comment.body.content,
-    slug
->>>>>>> d3a541b54c6af23757b034e0b24e965e251f2ff6
   ]);
 }
 
